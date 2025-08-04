34a5bb088ef7c567593d4fc07e7762f48b44f6d3
Mikha <me.mikhailrasyif@gmail.com>
2025-08-04 10:50:22 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,56 @@
+function CekJawaban() {
+    let skor=0
 
+    let jawaban1= document.getElementById("jawaban1").value;
+    if (jawaban.toLowerCase() === "Tokyo") {
+        skor++;
+    }
+
+        let jawaban2= document.getElementById("jawaban2").value;
+    if (jawaban.toLowerCase() === "Semarang") {
+        skor++;
+    }
+
+        let jawaban3= document.getElementById("jawaban3").value;
+    if (jawaban.toLowerCase() === "Nico Rosberg") {
+        skor++;
+    }
+
+        let jawaban4= document.getElementById("jawaban4").value;
+    if (jawaban.toLowerCase() === "120") {
+        skor++;
+    }
+    
+        let jawaban5= document.getElementById("jawaban5").value;
+    if (jawaban.toLowerCase() === "Athens") {
+        skor++;
+    }
+
+        let jawaban6= document.getElementById("jawaban6").value;
+    if (jawaban.toLowerCase() === "Calvin Coolidge") {
+        skor++;
+    }
+
+        let jawaban7= document.getElementById("jawaban7").value;
+    if (jawaban.toLowerCase() === "Yeti Wulandari") {
+        skor++;
+    }
+
+        let jawaban8= document.getElementById("jawaban8").value;
+    if (jawaban.toLowerCase() === "Bandung") {
+        skor++;
+    }
+
+        let jawaban9= document.getElementById("jawaban9").value;
+    if (jawaban.toLowerCase() === "IKN") {
+        skor++;
+    }
+
+        let jawaban10= document.getElementById("jawaban10").value;
+    if (jawaban.toLowerCase() === "Aku") {
+        skor++;
+    }
+
+    document.getElementById("Hasil").value;
+    innerText = "Skor Kamu " + skor + "/10"
+}
